dd5a2a27ce8f91cb5d181aeef24460df8bd8fc2e
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-10 20:08:21 +0300

    Create aavitka.lua

---
+++ aavitka.lua
@@ -0,0 +1,7 @@
+local Player = game:GetService("Players").LocalPlayer
+if Player.Name == "vetalikDragon1" then
+print("load scripts")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/aamaria.lua"))()
+else
+print("not maria")
+end

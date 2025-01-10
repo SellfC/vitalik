604888f91465a5dc2e112a1d8898c41f32ced659
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-10 20:09:33 +0300

    Create aavitka.lua

---
+++ aavitka.lua
@@ -0,0 +1,10 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+task.wait(5)
+script_key="uoTFuXKGtdOnWqEwDUbqdJDSmtiYkyAk";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e32274866a41a919678395d899e10bff.lua"))()
+task.wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/betterbuffer.lua"))()
+task.wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/confbettercard.lua"))()
+task.wait(1)
+print("aamaria.lua loaded successfully")

e5b5f3f2e8bd7efb1a5acf25d92ca488ff445b2f
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-10 20:20:41 +0300

    Update gamecheck.lua

--- gamecheck.lua
+++ gamecheck.lua
@@ -1,7 +1,8 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 if game.CreatorId == 10611639 then
-print("load scripts")
+setfpscap(8)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/vitalik/refs/heads/main/raspvitka.lua"))()
 else
+setfpscap(999)
 print(game.CreatorId)
 end

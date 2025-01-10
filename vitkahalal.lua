01855afb31ef85bd541d64e50ace967977e3b8ae
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-10 20:03:57 +0300

    Create vitkahalal.lua

---
+++ vitkahalal.lua
@@ -0,0 +1,21 @@
+getgenv().RequiredLevel = 40
+
+getgenv().HolidayHuntFarm = false
+getgenv().AutoBuyIcyStar = false
+
+getgenv().NightmareHuntFarm = false
+getgenv().AutoBuySpookyStar = false
+
+getgenv().AutoEquipBoostUnits = false
+getgenv().AutoOpenCapsule = false
+
+getgenv().AutoSellRareSkins = true
+getgenv().AutoSellEpicSkins = true
+getgenv().AutoSellLegendarySkins = true
+
+getgenv().WebhookURL = "https://discord.com/api/webhooks/1327169563921481728/6mqn5PptGY9AU_tTviT1ELVPLEawmJARUVG-gQ26E1Y8yNRQsgQ0zaxC5wKP-iZUEfEQ"
+getgenv().BoostFPS = true
+getgenv().Disable3DRender = true
+
+script_key="WHehLGHoRuIvWPZTcasKrLOLkXlRHnWj";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c8c006e970ed3323cef563220f3e3984.lua"))()
